c30d1f21a615118b5404ba4dacc6ffa224e7c191
Felicio Mununga <felicio@users.noreply.github.com>
2017-10-29 19:09:10 +0100

    Update main module

--- extension.js
+++ extension.js
@@ -1,3 +1,27 @@
 var vscode = require('vscode');
 
-var configuration = vscode.workspace.getConfiguration('javascript-snippets');
+var EXTENSION_IDENTIFIER = 'javascript-snippets';
+
+exports.activate = function activate(context) {
+  var configuration = vscode.workspace.getConfiguration(EXTENSION_IDENTIFIER);
+
+  var disposable = vscode.workspace.onDidChangeConfiguration(handleConfigurationChange, {
+    previousConfiguration: configuration,
+  });
+
+  context.subscriptions.push(disposable);
+};
+
+function handleConfigurationChange() {
+  var configuration = vscode.workspace.getConfiguration(EXTENSION_IDENTIFIER);
+
+  if (
+    configuration.get('semicolons') !==
+    this.previousConfiguration.get('semicolons')
+  ) {
+    // TODO: Export snippets as JSON.
+    console.log('semicolons: ' + configuration.get('semicolons'));
+  }
+
+  return configuration
+}
