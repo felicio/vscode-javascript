07f45ea7fde5754dc8df7a45350889208e21def9
Felicio Mununga <felicio@users.noreply.github.com>
2017-10-29 20:55:44 +0100

    Update main module

--- src/extension.js
+++ src/extension.js
@@ -1,27 +1,32 @@
+'use strict';
+
 var vscode = require('vscode');
 
 var EXTENSION_IDENTIFIER = 'javascript-snippets';
 
 exports.activate = function activate(context) {
   var configuration = vscode.workspace.getConfiguration(EXTENSION_IDENTIFIER);
+  var activeConfiguration = Object.assign({}, configuration);
 
-  var disposable = vscode.workspace.onDidChangeConfiguration(handleConfigurationChange, {
-    previousConfiguration: configuration,
+  var disposable = vscode.workspace.onDidChangeConfiguration(function() {
+    handleConfigurationChange(activeConfiguration);
   });
 
   context.subscriptions.push(disposable);
 };
 
-function handleConfigurationChange() {
-  var configuration = vscode.workspace.getConfiguration(EXTENSION_IDENTIFIER);
+function handleConfigurationChange(configuration) {
+  var key = 'semicolons';
+  var nextConfiguration = vscode.workspace.getConfiguration(
+    EXTENSION_IDENTIFIER,
+  );
 
-  if (
-    configuration.get('semicolons') !==
-    this.previousConfiguration.get('semicolons')
-  ) {
-    // TODO: Export snippets as JSON.
-    console.log('semicolons: ' + configuration.get('semicolons'));
+  var inspected = configuration.inspect(key);
+
+  if (configuration.get(key) !== nextConfiguration.get(key)) {
+    // TODO: Export src/snippets.js to out/snippets.json
+    console.log('semicolons: ' + nextConfiguration.get(key));
+    // FIXME: Does not override activeConfiguration variable
+    configuration = Object.assign({}, nextConfiguration);
   }
-
-  return configuration
 }
